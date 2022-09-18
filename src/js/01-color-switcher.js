d18ca0710f94b1cf48c156e0386c1c65487e6229
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-09-19 00:26:11 +0300

    commit 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const bodyEl = document.querySelector(`body`);
+const btnStart = document.querySelector(`[data-start]`);
+const btnStop = document.querySelector(`[data-stop]`);
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener(`click`, () => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    timerId = setInterval(() => {
+        bodyEl.style.background = getRandomHexColor();
+
+    }, 1000);
+});
+
+btnStop.addEventListener(`click`, () => {
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+    clearInterval(timerId);
+});
