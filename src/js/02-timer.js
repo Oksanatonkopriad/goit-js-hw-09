c26d091590140c16d7d83fdadfee7bf70d94fe3c
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-09-21 20:56:15 +0300

    02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,3 +2,84 @@
 import flatpickr from "flatpickr";
 // Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const inputDate = document.querySelector(`#datetime-picker`);
+const btnStart = document.querySelector(`[data-start]`);
+
+const daysNumber = document.querySelector(`[data-days]`);
+const hoursNumber = document.querySelector(`[data-hours]`);
+const minutesNumber = document.querySelector(`[data-minutes]`);
+const secondsNumber = document.querySelector(`[data-seconds]`);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+
+        if (selectedDates[0] <= Date.now()) {
+            Notiflix.Notify.failure("Please choose a date in the future");
+
+            return;
+        }
+
+        btnStart.addEventListener(`click`, () => {
+    timer.start();
+});
+  },
+};
+
+const fp = flatpickr(inputDate, options);
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+function pad(value) {
+    return String(value).padStart(2, '0')
+};
+
+pad();
+class Timer {
+    constructor() {
+        this.isActive = false;
+    }
+
+    start() {
+        if (this.isActive) {
+            return;
+        }
+        this.isActive = true;
+        setInterval(() => {
+            const deltaTime = fp.selectedDates[0] - Date.now();
+            const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+        daysNumber.textContent = `${pad(days)}`
+        hoursNumber.textContent = `${pad(hours)}`
+        minutesNumber.textContent = `${pad(minutes)}`
+        secondsNumber.textContent = `${pad(seconds)}`
+
+        }, 1000);
+    }
+};
+
+const timer = new Timer();
+
